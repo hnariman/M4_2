9676a34e92de12848bd0d1069783ba47b9ad9c81
SaLiGiAisBack <36777927+SaLiGiAisBack@users.noreply.github.com>
2020-04-16 17:50:48 +0400

    MVP second with nice design)

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,50 +1,36 @@
 import Calculator from "./calculate";
-
 import Task from "./Task";
+
+let counter = 0;
 class Application {
     constructor() {
-        document.querySelector('.add__target a').addEventListener('click', this.Run);
+        document.querySelector('.add__goal a').addEventListener('click', this.Run);
     }
-
     Run(e) {
         let name = document.querySelector('#task__name').value,
             sum  = Number(document.querySelector('#task__sum').value),
             term = Number(document.querySelector('#task__term').value),
-            container = document.querySelector('.container'),
-            wrapperBlock = document.querySelector('.block'),
+            allGoals = document.querySelector('.all__goals'),
             starterSum  = Number(document.querySelector('#task__start__sum').value);
         e.preventDefault();
         const task = new Task(name,sum,term,starterSum); // our inputs here
         const calculate = new Calculator();
         calculate.calc(task).then(
             result =>{
-                wrapperBlock.classList.add('task__wrapper');
-                let wrapper= document.createElement('DIV');
-                wrapper.classList.add('wrapper');
-                wrapperBlock.appendChild(wrapper);
-                let p = document.createElement('p');
-                p.innerHTML = task.name;
-                wrapper.appendChild(p);
-                let table = document.createElement('table');
-                table.innerHTML = `
-                    <tr>
-                        <td>Сумма</td>
-                        <td>${task.sum}</td>
-                    </tr>
-                     <tr>
-                        <td>Срок</td>
-                        <td>${task.term}</td>
-                    </tr>
-                     <tr>
-                        <td>Стартовая сумма</td>
-                        <td>${task.starterSum}</td>
-                    </tr>
-                     <tr>
-                        <td>Ежемесячное пополнение</td>
-                        <td>${result.toFixed(2)}</td>
-                    </tr>
+                let div = document.createElement('div');
+                div.classList.add('goal');
+                div.id = `task-${counter}`;
+                allGoals.append(div);
+                let desc = document.createElement('div');
+                desc.classList.add("goal-description");
+                desc.innerHTML =`
+                    <h3>${task.name}</h3>
+                    <p> до: июль, 2021 </p>
+                    <p> конечная сумма: ${task.sum} </p>
+                    <p> Ежемесячное пополнение: ${result.toFixed(2)} </p>
                 `;
-                wrapper.appendChild(table);
+                div.append(desc);
+                counter++;
             }
         )
     }
